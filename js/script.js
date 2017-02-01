850b86893d901d79b96cb41e181d7dc59c3b1c1e
Zac <zacaree@gmail.com>
2017-02-01 14:35:13 -0600

    >736 menu now works

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
 
-$('#product-btn').click(function(){
-    $('.drop-menu').fadeToggle(300);
-});
+    $(window).resize(function(){
+       // This will fire each time the window is resized:
+       if($(window).width() > 736){
+          $('.menu').show();
+       } else {
+          $('.menu').hide();
+       }
+    });
 
-$('.menu-icon').click(function(){
-    $('.dimmer').fadeToggle(300);
-    $('.menu').fadeToggle(300);
-});
 
+    $('.menu-icon').on('click', function(){
+        $('.overlay').fadeToggle(300);
+        $('.menu').fadeToggle(300);
+    });
+
+
+    $('#product-btn').click(function(){
+        $('.drop-menu').fadeToggle(300);
+    });
+
+
+    
 });
