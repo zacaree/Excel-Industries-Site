64b445c8ac6dda79bfeb964f8522132bd5eafa5f
Zac <zacaree@gmail.com>
2017-02-02 15:36:59 -0600

    jQuery menu interactions - wip

--- js/script.js
+++ js/script.js
@@ -10,24 +10,45 @@ $(document).ready(function(){
     });
 
 
+    // $('.menu-icon').click(function(){
+    //     $('.overlay, .menu').fadeIn(300);
+    //     $('.about-container').fadeOut(300);
+    // });
+
+
+    // $('#product-btn').click(function(){
+    //     $('.dropMenu').fadeToggle(300);
+    // });
+
+    // found menu toggle example here: http://jsfiddle.net/nick_craver/K6TSv/1/
+    //click toggle class and (this)next sibling & .overlay will fadeToggle
+
+    // $('.toggle').click(function(){
+    //     $(this).next().fadeToggle(300);
+    // });
+
     $('.menu-icon').click(function(){
-        $('.overlay, .menu').fadeToggle(300);
+        $('.menu, .overlay').fadeToggle(300);
+        $('.dropMenu, .hero-container, .about-container').fadeOut(300);
     });
 
+    // $('.toggle').click(function(){
+    //     $('.menu, .overlay').fadeToggle(300);
+    //     $('.dropMenu, .about-container').fadeOut(300);
+    // });
 
-    $('#product-btn').click(function(){
-        $('.drop-menu').fadeToggle(300);
-    });
 
-    $('#about-btn').click(function(){
+    $('.about-btn').click(function(){
         if($(window).width() < 736){
             $('.menu').fadeOut(300);
+            $('.hero-container').fadeOut(300);
             $('.overlay').fadeIn(300);
         }else{
+            $('.hero-container').fadeOut(300);
             $('.menu').fadeIn(300);
             $('.overlay').fadeIn(300);
         }
-        $('.about-container').fadeToggle(300);
+        $('.about-container').fadeIn(300);
     });
 
 });
