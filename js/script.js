41fe9d51139ddc9c018efff3bf7b1a4f01d3f0cb
Zac <zacaree@gmail.com>
2017-02-07 10:55:21 -0600

    All jQuery interactions are working

--- js/script.js
+++ js/script.js
@@ -1,44 +1,63 @@
 $(document).ready(function(){
 
     $(window).resize(function(){
-       // This will fire each time the window is resized:
-       if($(window).width() > 736){
-          $('.menu').show();
-       } else {
-          $('.menu').hide();
-       }
+        // This will fire each time the window is resized:
+        if($('.dropMenu').is(':visible')){
+            $('.product-btn').siblings().animate({top: '0px'}, 300);
+            $('.dropMenu').fadeOut();
+        }
+        if($(window).width() > 736){
+            $('.menu').fadeIn(300);
+            if($('.overlay', '.about-container').is(':visible')){
+                $('.overlay').fadeOut(300);
+                $('.hero-container').fadeIn(300);
+            }
+        } else { //if width is less than 763
+            $('.menu').hide();
+            $('.overlay').fadeOut(300);
+            $('.hero-container').fadeIn(300);
+            if($('.about-container').is(':visible')){
+                $('.about-container').fadeOut(300);
+            }
+        }
     });
-
-
-    // $('.menu-icon').click(function(){
-    //     $('.overlay, .menu').fadeIn(300);
-    //     $('.about-container').fadeOut(300);
-    // });
-
-
-    // $('#product-btn').click(function(){
-    //     $('.dropMenu').fadeToggle(300);
-    // });
-
-    // found menu toggle example here: http://jsfiddle.net/nick_craver/K6TSv/1/
-    //click toggle class and (this)next sibling & .overlay will fadeToggle
-
-    // $('.toggle').click(function(){
-    //     $(this).next().fadeToggle(300);
-    // });
 
     $('.menu-icon').click(function(){
-        $('.menu, .overlay').fadeToggle(300);
-        $('.dropMenu, .hero-container, .about-container').fadeOut(300);
+        if($('.menu').is(':hidden')){
+            $('.menu').fadeIn(300);
+            if($('.about-container').is(':visible')){
+                $('.about-container').fadeOut(300);
+            }else{
+                $('.hero-container').fadeOut(300);
+                $('.overlay').fadeIn(300);
+            }
+        }else{
+            $('.menu').fadeOut(300);
+            $('.overlay').fadeOut(300);
+            $('.hero-container').fadeIn(300);
+        }
     });
 
-    // $('.toggle').click(function(){
-    //     $('.menu, .overlay').fadeToggle(300);
-    //     $('.dropMenu, .about-container').fadeOut(300);
-    // });
-
+    $('.logo').click(function(){
+        if($('.dropMenu').is(':visible')){
+            $('.product-btn').siblings().animate({top: '0px'}, 300);
+            $('.dropMenu').fadeOut(300);
+        }
+        $('.overlay').fadeOut(300);
+        $('.about-container').fadeOut(300);
+        $('.hero-container').fadeIn(300);
+        if($(window).width() < 736){
+            if($('.menu').is(':visible')){
+                $('.menu').fadeOut(300);
+            }
+        }
+    });
 
     $('.about-btn').click(function(){
+        if($('.dropMenu').is(':visible')){
+            $('.product-btn').siblings().animate({top: '0px'}, 300);
+            $('.dropMenu').fadeOut(300);
+        }
         if($(window).width() < 736){
             $('.menu').fadeOut(300);
             $('.hero-container').fadeOut(300);
@@ -49,6 +68,20 @@ $(document).ready(function(){
             $('.overlay').fadeIn(300);
         }
         $('.about-container').fadeIn(300);
+    });
+
+    $('.product-btn').click(function(){
+        if($(window).width() < 736){
+            if($('.dropMenu').is(':hidden')){
+                $('.dropMenu').delay(200).fadeIn(300);
+                $(this).siblings().animate({top: '112px'}, 300);
+            }else{
+                $(this).siblings().delay(100).animate({top: '0px'}, 300);
+                $('.dropMenu').fadeOut(300);
+            }
+        }else{
+            $('.dropMenu').fadeToggle(300);
+        }
     });
 
 });
